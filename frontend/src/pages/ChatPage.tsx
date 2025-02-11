a68ac9c5f282a1f2bdb2574a2ee210282db3dfd3
MrCrowNFT <mrcrownft@gmail.com>
2025-02-11 18:48:15 -0300

    quick server

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,12 +1,14 @@
 const ChatPage = ()=>{
     return (
-        <>
+        <div className="flex content-center p-0 m-0">
         <div></div>
-        <form>
-            <input type="text" id="message-input"/>
+        <div className="w-full">
+        <form className="fixed flex mb-2.5 bg-white">
+            <input className="grow" type="text" id="message-input"/>
             <button type="submit" id="send-button">Send</button>
         </form>
-        </>
+        </div>
+        </div>
     )
 }
 
