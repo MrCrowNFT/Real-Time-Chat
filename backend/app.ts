d9fbfd0da106888f181e0d017688d4356b006ebc
MrCrowNFT <mrcrownft@gmail.com>
2025-02-11 19:39:05 -0300

    web socket server side connection

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,27 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, { cors: { origin: "*" } }); //cross-origin requests
 dotenv.config();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Home");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+  
+  socket.on("message", (msg) => {
+    console.log("Message received:", msg);
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => console.log("User disconnected:", socket.id)); //handle client disconnect
+});
 
 const PORT = process.env.PORT || 5500;
 
